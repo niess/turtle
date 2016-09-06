0b577d9b718fa91fe2ac7bd132a4c21f7cff2fa7
Valentin Niess <niess@in2p3.fr>
2016-09-06 16:47:43 +0200

    Update the licence info.

--- include/geotiff16.h
+++ include/geotiff16.h
@@ -1,5 +1,24 @@
 /*
- * Basic interface to geotiff files providing a reader for 16b data.
+ *  Topographic Utilities for Rendering The eLEvation (TURTLE)
+ *  Copyright (C) 2016  Valentin Niess
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Interface to geotiff files providing a reader for 16b data, e.g. ASTER-GDEM2
+ * tiles.
  */
 #ifndef GEOTIFF16_H
 #define GEOTIFF16_H
