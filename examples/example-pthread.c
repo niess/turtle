493689594c5210368770456fc5c1c4af183496d4
Valentin Niess <niess@in2p3.fr>
2016-09-07 22:12:06 +0200

    Update and document the pthread example.

--- examples/example-pthread.c
+++ examples/example-pthread.c
@@ -1,3 +1,10 @@
+/**
+ * This example illustrates the usage of a `turtle_client` in a multithreaded
+ * application using `pthread` and `mutex`. A set of `N_THREADS` clients
+ * concurrently access to the ASTER-GDEM2 elevation data over a 2x2 deg^2
+ * angular grid.
+ */
+
 /* C89 standard library. */
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,8 +16,14 @@
 /* The TURTLE API. */
 #include "turtle.h"
 
+/* The number of threads/clients to run. */
 #define N_THREADS 16
 
+/**
+ * The thread specific data are stored in a dedicated structure. Each thread
+ * will track the elevation data along a randomly generated line going from
+ * `(latitude_0, longitude_0)` to `(latitude_1, longitude_1)`.
+ */
 /* Storage for thread specific data. */
 struct thread_data {
 	pthread_t tid;
@@ -21,9 +34,17 @@ struct thread_data {
 	double longitude_1;
 };
 
+/**
+ * The geodetic datum is declared as a global static variable, to be seen
+ * by all threads/clients.
+ */
 /* Handle for the datum. */
 struct turtle_datum * datum = NULL;
 
+/**
+ * In order to lock critical sections a semaphore is used. The lock and
+ * unlock callbacks simply encapsulate the sem_wait and sem_post functions.
+ */
 /* Semaphore for locking critical sections. */
 static sem_t semaphore;
 
@@ -39,7 +60,11 @@ int unlock(void)
 	return sem_post(&semaphore);
 }
 
-/* Run a thread. */
+/**
+ * The following implements a threaded task. Each thread instanciates its own
+ * client in order to concurrently access the datum elevation data. Then the
+ * client is used to estimate the elevation at successive steps along a line.
+ */
 static void * run_thread(void * args)
 {
 	struct thread_data * data = (struct thread_data *)args;
@@ -68,7 +93,18 @@ clean_and_exit:
 	pthread_exit(0);
 }
 
-/* Draw a random number in [0;1] using the standard C library. */
+/**
+ * Finally the threads are spawned in the main function after various
+ * initialisation. The datum uses ASTER-GDEM2 elevation data. For this example
+ * to work, you'll need to extract the 4 tiles: `ASTGMT2_N45E002_dem.tif`,
+ * `ASTGMT2_N45E003_dem.tif`, `ASTGMT2_N46E002_dem.tif` and
+ * `ASTGMT2_N46E003_dem.tif`, to a folder named ASTGMT2.
+ *
+ * **Note** that since there are only 4 tiles that can be accessed, setting the
+ * datum `stack_size` to 4 or more will result in speed up since then there is
+ * no need to switch, i.e. unload and reload, tiles between threads.
+ */
+/* Draw a random number uniformly in [0;1] using the standard C library. */
 static double uniform(void)
 {
 	return ((double)rand())/RAND_MAX;
@@ -76,17 +112,23 @@ static double uniform(void)
 
 int main()
 {
-	/* Initialise. */
+	/* Initialise the semaphore, the TURTLE library and the datum. */
 	sem_init(&semaphore, 0, 1);
 	turtle_initialise();
-	enum turtle_return rc = turtle_datum_create("../turtle-data/ASTGTM2",
-		2, lock, unlock, &datum);
+	enum turtle_return rc = turtle_datum_create(
+		"../turtle-data/ASTGTM2", /* <= The elevation data folder. */
+		2,                        /* <= The stack size for tiles.  */
+		lock, unlock,             /* <= The lock/unlock callbacks. */
+		&datum);
 	if (rc != TURTLE_RETURN_SUCCESS) {
 		fprintf(stderr, "%s\n", turtle_strerror(rc));
 		goto clean_and_exit;
 	}
 
-	/* Create the threads. */
+	/*
+	 * Create the client threads and initialise the thread specific data
+	 * randomly.
+	 */
 	struct thread_data data[N_THREADS];
 	int i;
 	for (i = 0; i < N_THREADS; i++) {
@@ -115,7 +157,7 @@ int main()
 				turtle_strerror(data[i].rc));
 	}
 
-	/* Finalise. */
+	/* Finalise the semaphore and TURTLE. */
 clean_and_exit:
 	rc = turtle_datum_destroy(&datum);
 	if (rc != TURTLE_RETURN_SUCCESS)
